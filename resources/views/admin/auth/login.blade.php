09cc296ea1f5a9f1e3938f147b72b3583c28023a
Thuong Mai X <thuong.mai@monstar-lab.com>
2021-10-04 12:18:37 +0700

    feat: add structure for views

---
+++ resources/views/admin/auth/login.blade.php
@@ -0,0 +1,22 @@
+@extends('admin.layouts.auth')
+
+@section('authForm')
+    <form class="user" method="POST" action="{{ route('admin.login') }}">
+        @csrf
+        <div class="form-group">
+            <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail"
+                aria-describedby="emailHelp" placeholder="Enter Email Address...">
+        </div>
+        <div class="form-group">
+            <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword"
+                placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary btn-user btn-block">
+            Login
+        </button>
+        <hr>
+        <a href="https://www.google.com" class="btn btn-google btn-user btn-block">
+            <i class="fab fa-google fa-fw"></i> Login with Google
+        </a>
+    </form>
+@endsection
